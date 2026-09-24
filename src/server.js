6dd564066620663a2d918110f2d0dae41d6e63f0
Ngoc Linh <ngoclinh090608@gmail.com>
2026-09-24 23:05:16 +0700

    Dockerize product API

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${PORT}`);
   });
 };
